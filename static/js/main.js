d16dff46303de9dc788c5e7a8be628957b06f6c7
jakelashbrook <jakeashleylashbrook@gmail.com>
2021-06-06 20:12:15 +0000

    Initial Light/Dark Mode Functionality

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,26 @@
 $( document ).ready(function() {
+
+    $('#l-or-d-mode').click(function() {
+  if ($('body').hasClass('light-mode')){
+    $('body').removeClass('light-mode');
+    $('body').addClass('dark-mode');
+    $('#log-nav a').addClass('btn button m-2');
+    $('h3').addClass('dm-h3');
+    $('hr').addClass('hr-dm')
+    $('footer h6').addClass('dm-f-txt');
+    $('#l-or-d-mode').html('<i class="fas fa-moon"></i>');
+  }
+  else if ($('body').hasClass('dark-mode')){
+    $('body').removeClass('dark-mode');
+    $('body').addClass('light-mode');
+    $('#log-nav a').removeClass('btn button');
+    $('h3').removeClass('dm-h3');
+    $('hr').removeClass('hr-dm');
+    $('footer h6').removeClass('dm-f-txt');
+    $('#l-or-d-mode').html('<i class="fas fa-sun"></i>');
+  }
+});
+
     $('.edit-cn-btn').click(function() {
         $('#accordionExample').addClass('d-none');
         $('.edit-cn-area').removeClass('d-none');
